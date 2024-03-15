dd764fd48ee632833de3242a145d6169daaf0447
Spring2024 <spring2024@egr-eceloaner1-mbp.lan>
2024-03-15 10:54:45 -0400

    feat: project created

---
+++ Roommate/Roommate/RoommateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoommateApp.swift
+//  Roommate
+//
+//  Created by Spring2024 on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct RoommateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
