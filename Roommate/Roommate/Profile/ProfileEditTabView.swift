28d4c9b6077827dab92b4bb375b02910754b13e4
Spring2024 <spring2024@egr-eceloaner1-mbp.lan>
2024-04-04 09:16:15 -0400

    feat: finish upload feature

--- Roommate/Roommate/Profile/ProfileEditTabView.swift
+++ Roommate/Roommate/Profile/ProfileEditTabView.swift
@@ -18,6 +18,7 @@ struct ProfileEditTabView: View {
     @Binding var showOverlay: Bool
     var profileImages: [UIImage] = []
     @State private var selectedTab: Tab = .preview
+    @StateObject var uploadViewModel = UploadViewModel()
     
     var body: some View {
         VStack {
@@ -33,7 +34,9 @@ struct ProfileEditTabView: View {
                 HStack {
                     Spacer()
                     Button("Save") {
-                        // logic of saving
+                        if !uploadViewModel.upload(website: "http://vcm-39030.vm.duke.edu:8080/roommate/modify-profile", user: user) {
+                            print("Failed to start upload task")
+                        }
                         withAnimation {
                             showOverlay.toggle()
                         }
@@ -74,6 +77,55 @@ struct ProfileEditTabView: View {
         .background(.white)
     }
 }
+
+class UploadViewModel: NSObject, ObservableObject, URLSessionDelegate, URLSessionDataDelegate {
+    
+    @Published var showAlert: Bool = false
+    @Published var alertTitle: String = ""
+    @Published var alertInfo: String = ""
+    @Published var showUploadAlert: Bool = false
+    
+    func upload(website: String, user: User) -> Bool {
+        let url = URL(string: website)
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let session : URLSession = {
+            let config = URLSessionConfiguration.ephemeral
+            config.allowsCellularAccess = false
+            let session = URLSession(configuration: config, delegate: self, delegateQueue: .main)
+            return session
+        }()
+        
+        let encoder = JSONEncoder()
+        var jsonData: Data
+        do {
+            jsonData = try encoder.encode(user)
+        } catch {
+            return false
+        }
+        print(String(data: jsonData, encoding: .utf8)!)
+        request.httpBody = jsonData
+        let task = session.dataTask(with: request)
+        task.resume()
+        return true
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if error != nil {
+            self.alertTitle = "Upload Fail!"
+            self.alertInfo = "Please try again!"
+            self.showAlert = true
+        } else {
+            self.alertTitle = "Upload Success!"
+            self.alertInfo = "Please download again and check!"
+            self.showAlert = true
+            print("Upload successfully")
+        }
+    }
+}
+
+
 /*
  #Preview {
  editProfileView()
