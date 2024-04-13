78ce8695c4cc188ed18fc2c32a28dca208532dfb
tq22 <tq22@duke.edu>
2024-04-12 22:21:54 -0400

    feat: add delete friend feature in chat view

--- Roommate/Roommate/Chat/ChatView.swift
+++ Roommate/Roommate/Chat/ChatView.swift
@@ -11,6 +11,11 @@ struct ChatView: View {
     @ObservedObject var dataModel: Database  = Database.shared
     @Binding var user: User
     @ObservedObject var agoraManager: AgoraManager = AgoraManager.shared
+    @StateObject var uploadViewModel = UploadViewModel()
+    
+    var dedupedFriendList: [UUID] {
+        Array(Set(user.friends))
+    }
     
     var body: some View {
         VStack(spacing: 0) {
@@ -64,9 +69,29 @@ struct ChatView: View {
                     .foregroundColor(.black)
                     .padding(.vertical)
                 ) {
-                    ForEach(user.friends, id: \.self) { friend_uuid in
-                        let friend = dataModel.bindingForUser(friend_uuid)
-                        ChatRow(user: friend)
+                    // get a deduplicated friend uuid list
+                    
+                    ForEach(dedupedFriendList, id: \.self) { friend_uuid in
+                            let friend = dataModel.bindingForUser(friend_uuid)
+                            ChatRow(user: friend)
+                            .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                                Button {
+                                    let jsonObject = [
+                                        "myid": user.id.uuidString,
+                                        "matchid": friend_uuid.uuidString
+                                    ]
+                                    if let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: []) {
+                                        if !uploadViewModel.upload(website: "http://vcm-39030.vm.duke.edu:8080/roommate/delete", json: jsonData) {
+                                            print("Failed to start upload task")
+                                            return
+                                        }
+                                    }
+                                    user.friends.removeAll { $0 == friend.id }
+                                } label: {
+                                    Label("Delete", systemImage: "trash")
+                                }
+                                .tint(.red)
+                        }
                     }
                 }
             }
